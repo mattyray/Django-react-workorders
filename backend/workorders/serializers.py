6570946fe475a9a71e26cfc200b675dee0fc0851
mattyray <mnraynor90@gmail.com>
2025-04-25 17:32:15 -0400

    serializers.py

---
+++ backend/workorders/serializers.py
@@ -0,0 +1,9 @@
+# workorders/serializers.py
+
+from rest_framework import serializers
+from .models import WorkOrder
+
+class WorkOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WorkOrder
+        fields = '__all__'
