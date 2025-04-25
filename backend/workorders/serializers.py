3bdd880ee8cdf966617474c3db5ec2e3510975a4
mattyray <mnraynor90@gmail.com>
2025-04-25 17:32:24 -0400

    serializers.py

--- backend/workorders/serializers.py
+++ backend/workorders/serializers.py
@@ -1,9 +1,28 @@
 # workorders/serializers.py
 
 from rest_framework import serializers
-from .models import WorkOrder
+from .models import WorkOrder, Event, JobAttachment, JobNote
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+class JobAttachmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JobAttachment
+        fields = '__all__'
+
+class JobNoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JobNote
+        fields = '__all__'
 
 class WorkOrderSerializer(serializers.ModelSerializer):
+    events = EventSerializer(many=True, read_only=True)
+    attachments = JobAttachmentSerializer(many=True, read_only=True)
+    notes = JobNoteSerializer(many=True, read_only=True)
+
     class Meta:
         model = WorkOrder
         fields = '__all__'
