f0498307a09c646bc9457a3ea90be6997882dd08
mattyray <mnraynor90@gmail.com>
2025-04-25 15:20:20 -0400

    for the git cred

--- backend/clients/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.client_list, name='client_list'),
-    path('create/', views.client_create, name='client_create'),
-    path('<int:client_id>/', views.client_detail, name='client_detail'),
-    path('edit/<int:client_id>/', views.client_edit, name='client_edit'),
-]
