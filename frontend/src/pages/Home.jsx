e18c6bec890e38af6bcb3ea03449b3a2b1dbb6ae
mattyray <mnraynor90@gmail.com>
2025-04-28 13:32:50 -0400

    added placerholder code for home & workorders.jsx

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+      <div style={{ padding: "2rem", textAlign: "center" }}>
+        <h1>Welcome to the Work Orders App</h1>
+        <p>Manage your work orders efficiently and easily.</p>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
---
+++ frontend/src/pages/Workorders.jsx
@@ -0,0 +1,10 @@
+function WorkOrders() {
+    return (
+      <div style={{ padding: "2rem", textAlign: "center" }}>
+        <h1>Work Orders Page</h1>
+      </div>
+    );
+  }
+  
+  export default WorkOrders;
+  
