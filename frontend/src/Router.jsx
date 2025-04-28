2016d134b9bd8a9672fdfa730e23343844279169
mattyray <mnraynor90@gmail.com>
2025-04-28 13:27:20 -0400

    router.jsx = holds route definitions - shows navbar and footer loads the right page

---
+++ frontend/src/Router.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import WorkOrders from './pages/WorkOrders';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+function AppRouter() {
+  return (
+    <Router>
+      <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
+        <Navbar />
+        <div style={{ flex: "1" }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/workorders" element={<WorkOrders />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default AppRouter;
