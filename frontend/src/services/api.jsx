22884117f0c859afc7da8be2fc022ed22fae2a3d
mattyray <mnraynor90@gmail.com>
2025-04-29 13:27:21 -0400

    added services/apis.jsx added event to api.jsx mcode

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -5,3 +5,6 @@ const API_BASE = 'http://localhost:8002/api';
 
 export const fetchWorkOrders = () => axios.get(`${API_BASE}/workorders/`);
 export const createWorkOrder = (data) => axios.post(`${API_BASE}/workorders/`, data);
+export const createEvent = (workOrderId, data) =>
+    axios.post(`${API_BASE}/events/`, { ...data, work_order: workOrderId });
+  
