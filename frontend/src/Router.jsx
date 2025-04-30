4cd8bddc15b93a2645e964442814564f65693da8
mattyray <mnraynor90@gmail.com>
2025-04-30 11:25:27 -0400

    added tailwind to project - troubleshooting tailwind - main.jsx

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,24 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import WorkOrders from './pages/WorkOrders';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+  import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+  import Home from './pages/Home';
+  import WorkOrders from './pages/WorkOrders';
+  import Navbar from './components/Navbar';
+  import Footer from './components/Footer';
 
-function AppRouter() {
-  return (
-    <Router>
-      <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
-        <Navbar />
-        <div style={{ flex: "1" }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/workorders" element={<WorkOrders />} />
-          </Routes>
+  function AppRouter() {
+    return (
+      <Router>
+        <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
+          <Navbar />
+          <div style={{ flex: "1" }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/workorders" element={<WorkOrders />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </Router>
-  );
-}
+      </Router>
+    );
+  }
 
-export default AppRouter;
+  export default AppRouter;
