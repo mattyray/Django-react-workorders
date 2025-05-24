6705268c9b8838a6bbef47778d1819527dc43391
mattyray <mnraynor90@gmail.com>
2025-05-24 11:01:57 -0400

    frontend/src/api.jsx API_BASE = vite_api_base

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:8002/api';
+const API_BASE = import.meta.env.VITE_API_BASE;
 
 export const fetchWorkOrders = () => axios.get(`${API_BASE}/workorders/`);
 export const createWorkOrder = (data) => axios.post(`${API_BASE}/workorders/`, data);
